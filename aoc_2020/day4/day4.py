f315ceab8b08c2b36ae1b1f32047ff8f66331a14
Thibault  Vernier <thibaultvernier@Thibaults-MacBook-Pro.local>
2021-01-13 21:42:52 -0500

    finished and cleaned day4

--- aoc_2020/day4/day4.py
+++ aoc_2020/day4/day4.py
@@ -28,43 +28,62 @@ def analyze1(passports):
 
     return valid
 
+def validate_passport(passport):
+    items = passport.split()
+
+    if len(items) < 7:
+        return False
+    elif len(items) == 7 and "cid:" in passport:
+        print(passport)
+        return False
+
+    for item in items:
+        field, value = item.split(':')
+        if field == "byr":
+            if int(value) < 1920 or int(value) > 2002:
+                return False
+        elif field == "iyr":
+            if int(value) < 2010 or int(value) > 2020:
+                return False
+        elif field == "eyr":
+            if int(value) < 2020 or int(value) > 2030:
+                return False
+        elif field == "hgt":
+            if value[-2:] == "cm":
+                if int(value[:-2]) < 150 or int(value[:-2]) > 193:
+                    return False
+            elif value[-2:] == "in":
+                if int(value[:-2]) < 59 or int(value[:-2]) > 76:
+                    return False
+            else:
+                return False
+        elif field == "hcl":
+            if value[0] != "#":
+                return False
+            if len(value[1:]) == 6:
+                for i in range(1, len(value)):
+                    if value[i] not in "abcdef0123456789":
+                        return False
+            else:
+                return False
+        elif field == "ecl":
+            if value not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+                return False
+        elif field == "pid":
+            if len(value) != 9:
+                return False
+
+    return True
+
 def analyze2(passports):
-    invalid = 0
+    valid = 0
     for passport in passports:
-        items = passport.split()
-        flag = False
-        if len(items) == 8:
-            flag = True
-        elif "cid:" not in passport and len(items) >= 7:
-            flag = True
-
-        if flag == True:
-            for item in items:
-                field, value = item.split(':')
-                if field == "byr" and int(value) < 1920 or int(value) > 2002:
-                    invalid += 1
-                    break
-                elif field == "iyr" and int(value) < 2010 or int(value) > 2020:
-                    invalid += 1
-                    break
-                elif field == "eyr" and int(value) < 2020 or int(value) > 2030:
-                    invalid += 1
-                    break
-                elif field == "hgt":
-                    if value[-2:] == "cm" and int(value[:-2]) < 150 or int(value[:-2]) > 193:
-                        invalid += 1
-                        break
-                    elif value[-2:] == "in" and int(value[:-2]) < 59 or int(value[:-2]) > 76:
-                        invalid += 1
-                        break
-                elif field == "hcl":
-        else:
-            invalid += 1
-
-
-    return len(passports) - invalid
+        if validate_passport(passport):
+            valid += 1
+    return valid
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     passports = read_file(filename)
     print(analyze1(passports))
+    print(analyze2(passports))
